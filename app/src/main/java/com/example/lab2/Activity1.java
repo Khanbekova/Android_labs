436f05f456d1b153239c43f9e71244365eb369fa
Khanbekova <lenahanbekova77@gmail.com>
2022-10-24 00:37:07 +0300

    lab4

--- app/src/main/java/com/example/lab2/Activity1.java
+++ app/src/main/java/com/example/lab2/Activity1.java
@@ -1,7 +1,9 @@
 package com.example.lab2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -10,6 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class Activity1 extends Activity {
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final int APP_PREFERENCES_THEME = 1;
+    int positionTheme;
     @Override
     protected void onStart() {
         super.onStart();
@@ -20,18 +25,22 @@ public class Activity1 extends Activity {
         super.onResume();
         Log.d("Act1Tag" , "OnResume");
     }
+    @Override
     protected void onPause() {
         super.onPause();
         Log.d("Act1Tag" , "onPause");
     }
+    @Override
     protected void onStop() {
         super.onStop();
         Log.d("Act1Tag" , "onStop");
     }
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d("Act1Tag" , "onDestroy");
     }
+    @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("Act1Tag" , "onRestart");
@@ -40,30 +49,54 @@ public class Activity1 extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPrefTheme = this.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
 
+        positionTheme = sharedPrefTheme.getInt("position", 1);
+        Log.d("Act1TagSTYLE" , String.valueOf(positionTheme));
+        if (positionTheme == 0){
+            setTheme(R.style.ThemeNight_Lab2);
+            Log.d("Act1TagSTYLE" , "NIGHT_MODE");
+        }
+        else{
+            setTheme(R.style.ThemeLight_Lab2);
+        }
 
         setContentView(R.layout.activity_act1);
         EditText personName = findViewById(R.id.editTextTextPersonName);
         EditText password = findViewById(R.id.editTextTextPassword);
 
+        SharedPreferences sharedPref1 = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref2 = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor1 = sharedPref1.edit();
+        SharedPreferences.Editor editor2 = sharedPref2.edit();
+        personName.setText(sharedPref1.getString("text1",""));
+        password.setText(sharedPref2.getString("text2",""));
+
         Button btnActTwo = findViewById(R.id.button);
+
+
         super.onCreate(savedInstanceState);
         Log.d("Act1Tag" , "onCreate");
         btnActTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-               if ((TextUtils.isEmpty(personName.getText()))&&(TextUtils.isEmpty(password.getText()))){
-                   return;
+                if ((TextUtils.isEmpty(personName.getText()))&&(TextUtils.isEmpty(password.getText()))){
+                    return;
                 }
-               else {
-                   if ((personName.getText().toString().equals("Elena"))&&(password.getText().toString().equals("1234"))) {
-                       Intent intent = new Intent(Activity1.this, Activity2.class);
-                       intent.putExtra("hello", (personName.getText().toString()));
-                       Log.d("Act1Tag" , "CallAct2");
-                       startActivity(intent);
-                   }
-               }
+                else {
+                    if ((personName.getText().toString().equals("Elena"))&&(password.getText().toString().equals("1234"))) {
+
+                        editor1.putString("text1", personName.getText().toString());
+                        editor1.apply();
+                        editor2.putString("text2", password.getText().toString());
+                        editor2.apply();
+                        Intent intent = new Intent(Activity1.this, Activity2.class);
+                        intent.putExtra("hello", (personName.getText().toString()));
+                        Log.d("Act1Tag" , "CallAct2");
+                        startActivity(intent);
+                    }
+                }
             }
         });
 
--- app/src/main/java/com/example/lab2/Activity2.java
+++ app/src/main/java/com/example/lab2/Activity2.java
@@ -1,6 +1,8 @@
 package com.example.lab2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +17,9 @@ import java.util.ArrayList;
 
 
 public class Activity2 extends AppCompatActivity {
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final int APP_PREFERENCES_THEME = 1;
+    int positionTheme;
     @Override
     protected void onStart() {
         super.onStart();
@@ -43,10 +48,22 @@ public class Activity2 extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPrefTheme =  this.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor1 = sharedPrefTheme.edit();
+        positionTheme = sharedPrefTheme.getInt("position", 1);
+        Log.d("Act2TagSTYLE" , String.valueOf(positionTheme));
 
 
-        Bundle arguments = getIntent().getExtras();
-        String user = arguments.get("hello").toString();
+        if (positionTheme == 0){
+            setTheme(R.style.ThemeNight_Lab2);
+        }
+        else{
+            setTheme(R.style.ThemeLight_Lab2);
+        }
+        Log.d("Act2TagSTYLE" , String.valueOf(positionTheme));
+
+        Bundle bundle = getIntent().getExtras();
+        String user = bundle.get("hello").toString();
 
         setContentView(R.layout.activity_2);
         ArrayList<String> StringArray = new ArrayList<String>();
@@ -55,13 +72,15 @@ public class Activity2 extends AppCompatActivity {
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
         Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+
         ListView listView = findViewById(R.id.textList);
 
         TextAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, StringArray);
         listView.setAdapter(TextAdapter);
         StringArray.add(user);
         super.onCreate(savedInstanceState);
-        Log.d("Act2Tag" , "onCreate");
+        Log.d("Act2TagSTYLE" , "onCreate");
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,6 +95,24 @@ public class Activity2 extends AppCompatActivity {
                 Intent intent = new Intent(Activity2.this, Activity1.class);
                 Log.d("Act2Tag" , "CallAct1");
                 startActivity(intent);
+
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (positionTheme == 0){
+                    setTheme(R.style.ThemeNight_Lab2);
+                    positionTheme = 1;
+                }
+                else{
+                    setTheme(R.style.ThemeLight_Lab2);
+                    positionTheme = 0;
+                }
+                editor1.putInt("position",positionTheme );
+                editor1.apply();
+                Log.d("Act2Tag" , String.valueOf(positionTheme));
+                recreate();
 
             }
         });
